28233e5f47bf5c61d1f1011944008cd1a1bad593
Glitch (glitch-hello-website) <none>
2022-12-14 22:33:21 +0000

    🛥🍍 Checkpoint ./server.js:51959287/196

--- server.js
+++ server.js
@@ -8,15 +8,6 @@ const io = require('socket.io')(http);
 
 var server = require('http').createServer(app);
 
-
-// NOT WORKING
-// const database = require('./database.js')
-// uncommenting this will crash the app, 
-// there are some odd dependency issues 
-// regarding this version of node and 
-// mongoDB
-
-
 const users = []
 
 var currUser
